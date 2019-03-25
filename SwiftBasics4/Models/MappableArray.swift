0dbdd9b25311225006a19476e7da0ddea2c940d3
houwei <houwei@hwdeMac.local>
2019-03-25 15:32:53 +0800

    ,

--- SwiftBasics4/Models/MappableArray.swift
+++ SwiftBasics4/Models/MappableArray.swift
@@ -7,22 +7,23 @@ import Foundation
 import ObjectMapper
 
 class MappableArray<T: Mappable> : Mappable {
-
+    
     var value: [T] = []
-
+    
     required init?(map: Map) {
-        if let val = map.currentValue as? [String:Any] {
+        if let val = map.currentValue as? [[String:Any]] {
             for item in val {
-                let _map = Map(mappingType:.fromJSON, JSON: item)
+                let _map = Map(mappingType: .fromJSON, JSON: item)
                 var obj = T(map:_map)
                 obj?.mapping(map:_map)
                 value.append(obj!)
             }
         }
     }
-
+    
     func mapping(map: Map) {
         // nothing
         value <- map["value"]
     }
 }
+
